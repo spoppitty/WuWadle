a5d2f5b8b1aab90a601ca68c50867d63fca2a918
jkke123 <128205296+jkke123@users.noreply.github.com>
2026-07-08 04:02:07 -0700

    firebase setup

---
+++ public/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABUA28g3o4fQJoQYlfkyD5hE2gEgRKpy8",
+  authDomain: "wuwadle.firebaseapp.com",
+  projectId: "wuwadle",
+  storageBucket: "wuwadle.firebasestorage.app",
+  messagingSenderId: "228676733862",
+  appId: "1:228676733862:web:5b76969175e994cc2bdd67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
