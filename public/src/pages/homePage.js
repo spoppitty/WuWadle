a4062c6330ee0d242e176597ed937eda16306840
jkke123 <128205296+jkke123@users.noreply.github.com>
2026-07-08 22:33:57 -0700

    new resonators.js

--- public/src/pages/homePage.js
+++ public/src/pages/homePage.js
@@ -1,37 +1,6 @@
-const app = document.getElementById("app");
+import resonators from "../resonators.js";
 
-const resonators = [
-  {
-    name: "/src/assets/chixiaIcon.png",
-    altName: "Chixia",
-    element: "Fusion",
-    weaponType: "Pistols",
-    gender: "Female",
-    region: "Huanglong",
-    rarity: 4,
-    releaseYear: 2024
-  },
-  {
-    name: "/src/assets/jiyanIcon.png",
-    altName: "Jiyan",
-    element: "Aero",
-    weaponType: "Broadblade",
-    gender: "Male",
-    region: "Huanglong",
-    rarity: 5,
-    releaseYear: 2024
-  },
-  {
-    name: "/src/assets/yinlinIcon.png",
-    altName: "Yinlin",
-    element: "Electro",
-    weaponType: "Rectifier",
-    gender: "Female",
-    region: "Huanglong",
-    rarity: 5,
-    releaseYear: 2024
-  }
-];
+const app = document.getElementById("app");
 
 app.innerHTML = `
   <main>
---
+++ public/src/resonators.js
@@ -0,0 +1,34 @@
+const resonators = [
+  {
+    name: "/src/assets/chixiaIcon.png",
+    altName: "Chixia",
+    element: "Fusion",
+    weaponType: "Pistols",
+    gender: "Female",
+    region: "Huanglong",
+    rarity: 4,
+    releaseYear: 2024
+  },
+  {
+    name: "/src/assets/jiyanIcon.png",
+    altName: "Jiyan",
+    element: "Aero",
+    weaponType: "Broadblade",
+    gender: "Male",
+    region: "Huanglong",
+    rarity: 5,
+    releaseYear: 2024
+  },
+  {
+    name: "/src/assets/yinlinIcon.png",
+    altName: "Yinlin",
+    element: "Electro",
+    weaponType: "Rectifier",
+    gender: "Female",
+    region: "Huanglong",
+    rarity: 5,
+    releaseYear: 2024
+  }
+];
+
+export default resonators;
