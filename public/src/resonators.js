ae6fba0b073175abf98c9d1ea804632125b06c6f
jkke123 <128205296+jkke123@users.noreply.github.com>
2026-07-10 18:37:12 -0700

    finished resonators

--- public/src/resonators.js
+++ public/src/resonators.js
@@ -1,25 +1,7 @@
+import { first } from "firebase/firestore/pipelines";
+
 const resonators = [
   
-  {
-    icon: "/src/assets/iconJiyan.png",
-    name: "Jiyan",
-    element: "Aero",
-    weaponType: "Broadblade",
-    gender: "Male",
-    firstAppearance: "Huanglong",
-    rarity: 5,
-    releaseYear: 2024
-  },
-  {
-    icon: "/src/assets/iconYinlin.png",
-    name: "Yinlin",
-    element: "Electro",
-    weaponType: "Rectifier",
-    gender: "Female",
-    firstAppearance: "Huanglong",
-    rarity: 5,
-    releaseYear: 2024
-  }, 
   {
     icon: "/src/assets/iconAalto.png",
     name: "Aalto",
@@ -196,10 +178,361 @@ const resonators = [
     element: "Fusion",
     weaponType: "Rectifier",
     gender: "Female",
-    firstAppearance: "i dunno",
+    firstAppearance: "Huanglong",
     rarity: 5,
     releaseYear: 2024
   },
+  {
+    icon: "/src/assets/iconGalbrena.png",
+    name: "Galbrena",
+    element: "Fusion",
+    weaponType: "Pistols",
+    gender: "Female",
+    firstAppearance: "Septimont",
+    rarity: 5,
+    releaseYear: 2025
+  },
+  {
+    icon: "/src/assets/iconHiyuki.png",
+    name: "Hiyuki",
+    element: "Glacio",
+    weaponType: "Sword",
+    gender: "Female",
+    firstAppearance: "Lahai Roi",
+    rarity: 5,
+    releaseYear: 2026
+  },
+  {
+    icon: "/src/assets/iconIuno.png",
+    name: "Iuno",
+    element: "Aero",
+    weaponType: "Gauntlets",
+    gender: "Female",
+    firstAppearance: "Septimont",
+    rarity: 5,
+    releaseYear: 2025
+  },
+  {
+    icon: "/src/assets/iconJianxin.png",
+    name: "Jianxin",
+    element: "Aero",
+    weaponType: "Gauntlets",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 5,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconJinshi.png",
+    name: "Jinshi",
+    element: "Spectro",
+    weaponType: "Broadblade",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 5,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconJiyan.png",
+    name: "Jiyan",
+    element: "Aero",
+    weaponType: "Broadblade",
+    gender: "Male",
+    firstAppearance: "Huanglong",
+    rarity: 5,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconLingyang.png",
+    name: "Lingyang",
+    element: "Glacio",
+    weaponType: "Gauntlets",
+    gender: "Male",
+    firstAppearance: "Huanglong",
+    rarity: 5,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconLucilla.png",
+    name: "Lucilla",
+    element: "Glacio",
+    weaponType: "Rectifier",
+    gender: "Female",
+    firstAppearance: "Lahai Roi",
+    rarity: 5,
+    releaseYear: 2026
+  },
+  {
+    icon: "/src/assets/iconLucy.png",
+    name: "Lucy",
+    element: "Spectro",
+    weaponType: "Pistols",
+    gender: "Female",
+    firstAppearance: "Cyberpunk Collab",
+    rarity: 5,
+    releaseYear: 2026
+  },
+  {
+    icon: "/src/assets/iconLumi.png",
+    name: "Lumi",
+    element: "Electro",
+    weaponType: "Broaddblade",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 4,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconLupa.png",
+    name: "Lupa",
+    element: "Fusion",
+    weaponType: "Broadblade",
+    gender: "Female",
+    firstAppearance: "Septimont",
+    rarity: 5,
+    releaseYear: 2025
+  },
+  {
+    icon: "/src/assets/iconLuuk_Herssen.png",
+    name: "Luuk Herssen",
+    element: "Spectro", 
+    weaponType: "Gauntlets",
+    gender: "Male",
+    firstAppearance: "Lahai Roi",
+    rarity: 5,
+    releaseYear: 2026
+  },
+  {
+    icon: "/src/assets/iconLynae.png",
+    name: "Lynae",
+    element: "Spectro",
+    weaponType: "Pistols",
+    gender: "Female",
+    firstAppearance: "Lahai Roi",
+    rarity: 5,
+    releaseYear: 2025
+  },
+  {
+    icon: "/src/assets/iconMornye.png",
+    name: "Mornye",
+    element: "Fusion",
+    weaponType: "Broadblade",
+    gender: "Female",
+    firstAppearance: "Lahai Roi",
+    rarity: 5, 
+    releaseYear: 2026
+  },
+  {
+    icon: "/src/assets/iconMortefi.png",
+    name: "Mortefi",
+    element: "Fusion",
+    weaponType: "Pistols",
+    gender: "Male",
+    firstAppearance: "Huanglong",
+    rarity: 4, 
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconPhoebe.png",
+    name: "Phoebe",
+    element: "Spectro",
+    weaponType: "Rectifier",
+    gender: "Female",
+    firstAppearance: "Ragunna",
+    rarity: 5,
+    releaseYear: 2025
+  },
+  {
+    icon: "/src/assets/iconPhrolova.png",
+    name: "Phrolova",
+    element: "Havoc",
+    weaponType: "Rectifier",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 5,
+    releaseYear: 2025
+  },
+  {
+    icon: "/src/assets/iconQiuyuan.png",
+    name: "Qiuyuan",
+    element: "Aero",
+    weaponType: "Sword",
+    gender: "Male",
+    firstAppearance: "Septimont",
+    rarity: 5,
+    releaseYear: 2025
+  },
+  {
+    icon: "/src/assets/iconRebecca.png",
+    name: "Rebecca",
+    element: "Electro",
+    weaponType: "Pistols",
+    gender: "Female",
+    firstAppearance: "Cyberpunk Collab",
+    rarity: 5,
+    releaseYear: 2026
+  },
+  {
+    icon: "/src/assets/iconRoccia.png",
+    name: "Roccia",
+    element: "Havoc",
+    weaponType: "Gauntlets",
+    gender: "Female",
+    firstAppearance: "Ragunna",
+    rarity: 5,
+    releaseYear: 2025
+  },
+  {
+    icon: "/src/assets/iconRover.png", 
+    name: "Rover",
+    element: "Multiple",
+    weaponType: "Sword",
+    gender: "Can be Male/Female",
+    firstAppearance: "Huanglong",
+    rarity: 5,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconSanhua.png",
+    name: "Sanhua",
+    element: "Glacio",
+    weaponType: "Sword",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 4, 
+    releaseYear: 2024
+  },
+  { 
+    icon: "/src/assets/iconShorekeeper.png",
+    name: "Shorekeeper",
+    element: "Spectro",
+    weaponType: "Rectifier",
+    gender: "Female",
+    firstAppearance: "Black Shores",
+    rarity: 5,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconSigrika.png",
+    name: "Sigrika",
+    element: "Aero",
+    weaponType: "Gauntlents",
+    gender: "Female",
+    firstAppearance: "Lahai Roi",
+    rarity: 5,
+    releaseYear: 2026
+  },
+  {
+    icon: "/src/assets/iconSuisui.png",
+    name: "Suisui",
+    element: "Glacio",
+    weaponType: "Rectifier",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 5, 
+    releaseYear: 2026
+  },
+  {
+    icon: "/src/assets/iconTaoqi.png",
+    name: "Taoqi",
+    element: "Havoc",
+    weaponType: "Broadblade",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 4,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconVerina.png",
+    name: "Verina",
+    element: "Spectro",
+    weaponType: "Rectifier",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 5,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconXiangli_Yao.png",
+    name: "Xiangli Yao",
+    element: "Electro",
+    weaponType: "Gauntlets",
+    gender: "Male",
+    firstAppearance: "Huanglong",
+    rarity: 5,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconYangyang.png",
+    name: "Yangyang",
+    element: "Aero",
+    weaponType: "Sword",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 4, 
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconYangyangXuanling.png",
+    name: "Yangyang: Xuanling",
+    element: "Havoc",
+    weaponType: "Sword",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 5,
+    releaseYear: 2026
+  },
+  {
+    icon: "/src/assets/iconYinlin.png",
+    name: "Yinlin",
+    element: "Electro",
+    weaponType: "Rectifier",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 5,
+    releaseYear: 2024
+  }, 
+  {
+    icon: "/src/assets/iconYouhu.png",
+    name: "Youhua",
+    element: "Glacio",
+    weaponType: "Gauntlets",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 4,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconYuanwu.png",
+    name: "Yuanwu",
+    element: "Electro",
+    weaponType: "Gauntlets",
+    gender: "Male",
+    firstAppearance: "Huanglong",
+    rarity: 4,
+    releaseYear: 2024
+  },
+  {
+    icon: "/src/assets/iconZani.png",
+    name: "Zani",
+    element: "Spectro",
+    weaponType: "Gauntlets",
+    gender: "Female",
+    firstAppearance: "Ragunna",
+    rarity: 5,
+    releaseYear: 2025
+  },
+  {
+    icon: "/src/assets/iconZhezhi.png",
+    name: "Zhezhi",
+    element: "Glacio",
+    weaponType: "Rectifier",
+    gender: "Female",
+    firstAppearance: "Huanglong",
+    rarity: 5, 
+    releaseYear: 2024
+  }
+
 ];
 
 export default resonators;
